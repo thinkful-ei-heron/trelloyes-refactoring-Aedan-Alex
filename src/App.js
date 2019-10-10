1f1d25b3bb10d55e8214fe7cc9d44e4571e7dc26
Just-A-Fool <aedanscarn@gmail.com>
2019-10-10 10:36:49 -0600

    fixed delete function

--- src/App.js
+++ src/App.js
@@ -10,48 +10,95 @@ class App extends Component {
     }
   };
 
-  state = { store: {
-    lists: [
-      {
-        id: '1',
-        header: 'First list',
-        cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+  state = { 
+    store: {
+      lists: [
+        {
+          id: '1',
+          header: 'First list',
+          cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+        },
+        {
+          id: '2',
+          header: 'Second list',
+          cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+        },
+        {
+          id: '3',
+          header: 'Third list',
+          cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+        },
+        {
+          id: '4',
+          header: 'Fourth list',
+          cardIds: [ 'l', 'm' ],
+        },
+      ],
+      allCards: {
+        'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+        'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+        'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+        'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+        'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+        'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+        'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+        'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+        'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+        'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+        'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+        'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+        'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
       },
-      {
-        id: '2',
-        header: 'Second list',
-        cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
-      },
-      {
-        id: '3',
-        header: 'Third list',
-        cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
-      },
-      {
-        id: '4',
-        header: 'Fourth list',
-        cardIds: [ 'l', 'm' ],
-      },
-    ],
-    allCards: {
-      'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
-      'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
-      'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
-      'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
-      'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
-      'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
-      'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
-      'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
-      'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
-      'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
-      'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
-      'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
-      'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
-    },
   }
 }
 
-  
+  omit(obj, keyToOmit) {
+    return Object.entries(obj).reduce(
+      (newObj, [key, value]) =>
+          key === keyToOmit ? newObj : {...newObj, [key]: value},
+      {}
+    );
+  }
+
+  handleDelete = (id) => {
+    let newLists = this.state.store.lists.map(list => {
+      let newList = list.cardIds.filter(item => item !== id)
+      return {
+        id: list.id,
+        header: list.header,
+        cardIds: newList
+      };
+    });
+
+    let newCards = this.omit(this.state.store.allCards, id);
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: newCards
+      }
+    });
+  }
+
+  newRandomCard() {
+    const id = Math.random().toString(36).substring(2, 4)
+      + Math.random().toString(36).substring(2, 4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: 'lorem ipsum'
+    }
+  }
+
+  handleNewCard = () => {
+    let newCard = this.newRandomCard()
+    let newArray = [...this.state.cards, newCard]
+    this.setState ({
+      cards: newArray
+    })
+
+  }
+
   
 
 
@@ -68,6 +115,7 @@ class App extends Component {
               key={list.id}
               header={list.header}
               cards={list.cardIds.map(id => this.state.store.allCards[id])}
+              deleteItem = {this.handleDelete}
             />
           ))}
         </div>
--- src/List.js
+++ src/List.js
@@ -2,37 +2,37 @@ import React from 'react';
 import Card from './Card'
 import './List.css';
 
-class List extends React.Component {
+function List(props) {
 
-  state = { cards: this.props.cards,
-            header: this.props.header
-  }
+  // state = { cards: this.props.cards,
+  //           header: this.props.header
+  // }
   
-  handleDelete = (id) => {
-    let newArray = this.state.cards.filter(item => item.id !== id);
-    this.setState({
-      cards: newArray
-    });
-  }
+  // handleDelete = (id) => {
+  //   let newArray = this.state.cards.filter(item => item.id !== id);
+  //   this.setState({
+  //     cards: newArray
+  //   });
+  // }
 
-  newRandomCard() {
-    const id = Math.random().toString(36).substring(2, 4)
-      + Math.random().toString(36).substring(2, 4);
-    return {
-      id,
-      title: `Random Card ${id}`,
-      content: 'lorem ipsum'
-    }
-  }
+  // newRandomCard() {
+  //   const id = Math.random().toString(36).substring(2, 4)
+  //     + Math.random().toString(36).substring(2, 4);
+  //   return {
+  //     id,
+  //     title: `Random Card ${id}`,
+  //     content: 'lorem ipsum'
+  //   }
+  // }
 
-  handleNewCard = () => {
-    let newCard = this.newRandomCard()
-    let newArray = [...this.state.cards, newCard]
-    this.setState ({
-      cards: newArray
-    })
+  // handleNewCard = () => {
+  //   let newCard = this.newRandomCard()
+  //   let newArray = [...this.state.cards, newCard]
+  //   this.setState ({
+  //     cards: newArray
+  //   })
 
-  }
+  // }
 
   // const newRandomCard = () => {
   //   const id = Math.random().toString(36).substring(2, 4)
@@ -50,33 +50,32 @@ class List extends React.Component {
 
 
 
-  render(props) {
+  
     return (
       <section className='List'>
         <header className='List-header'>
-          <h2>{this.state.header}</h2>
+          <h2>{props.header}</h2>
         </header>
         <div className='List-cards'>
-          {this.state.cards.map((card) =>
+          {props.cards.map((card) =>
             <Card
               key={card.id}
               id = {card.id}
               title={card.title}
               content={card.content}
-              deleteItem={this.handleDelete}
+              deleteItem={props.deleteItem}
             />
           )}
           <button
             type='button'
             className='List-add-button'
-            onClick={this.handleNewCard}
           >
             + Add Random Card
           </button>
         </div>
       </section>
     )
-  }
+  
 }
 
 export default List;
